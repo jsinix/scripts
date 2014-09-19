b76280db94849c77d81ecc72e3e157f03893d3a9
jsinix <jsinix@users.noreply.github.com>
2014-09-18 20:58:01 -0400

    updating with encryption
    updating with some changes including symmetric encryption.

--- jsinix_gmailer/jsinix_gmailer.pl
+++ jsinix_gmailer/jsinix_gmailer.pl
@@ -15,6 +15,8 @@ use Term::ReadKey;
 use Switch;
 use Mail::IMAPClient;
 use Term::ANSIColor qw(:constants);
+use Crypt::DES;
+use Crypt::Lite;
 
 # Declaring all variables
 my $cgi;
@@ -34,15 +36,32 @@ my $emaillogin;
 my $uname1;
 my $pass1;
 my $choice = '';
+my $emailbodyfin;
+my $emailbodyplain;
+my $enckey1;
+my $cipher;
+my $ciphertext1;
+my $encrypted;
+my $crypt;
+my $emailbodyplain1;
+my $enckey2;
+my $decryptedmsg = '';
+my $dcrypt;
+my $decrypted;
+my $encryptedmsg = '';
+my $enckey3;
+my $emailbodyplain2;
 
-while ($choice ne '3') {
+while ($choice ne '5') {
 	START:
 	system("clear");
 	
 	print BOLD, BLUE, "\n\n\n\n\tJSINIX Gmailer\n\n", RESET;
 	print "\n\t1. Send email".
-	      "\n\t2. Check email\n".
-	      "\n\t3. Quit(q)";	
+	      "\n\t2. Check email".
+	      "\n\t3. Encrypt email".	
+	      "\n\t4. Decrypt email".	
+	      "\n\t5. Quit(q)";	
 	
 	print "\n\n\tChoice: ";
 	$choice = <STDIN>;
@@ -68,6 +87,25 @@ while ($choice ne '3') {
 
 		case '3'
 		{
+                        system("clear");
+                        mail_encrypt();
+                        print "\n\tPress enter to continue"; <STDIN>;
+                        goto START;
+			
+		}
+
+		case '4'
+		{
+                        system("clear");
+                        mail_decrypt();
+			#encdec('test','test');
+                        print "\n\tPress enter to continue"; <STDIN>;
+                        goto START;
+		
+		}
+
+		case '5'
+		{
 			system("clear");
                         exit();
 		}
@@ -88,8 +126,6 @@ sub mail_check_pop {
 
 	$mailhost = 'pop.gmail.com';
 	$port = '995';
-
-	#print $cgi->header();
 
 	$pop = Mail::POP3Client->new(USER=> $uname1,
 		PASSWORD => $pass1,
@@ -130,15 +166,35 @@ sub mail_check_pop {
 mail_send();
 sub mail_send {
 
-	my ($uname2, $pass2) = get_credentials();
+	print "\n\tLogin ID: ";
+	my $uname2 = get_user();
+	print "\n\tPassword: ";
+	my $pass2 = get_pass();
 
 	print "\n\tMail To: ";
 	my $emailto = <STDIN>;
 	chomp($emailto);
 
 	print "\n\tBody: ";
-	my $emailbody = <STDIN>;
-	chomp($emailbody);
+	$emailbodyplain = get_body(); 
+
+	print "\n\tWant to encrypt body ?(y/n)";
+	my $echoice = <STDIN>;
+	chomp($echoice);
+
+	if ($echoice eq 'y') {
+		
+		print "\n\tEncryption key: ";
+		$enckey1 = get_pass();	
+		$emailbodyfin = encrypt_body($emailbodyplain, $enckey1);
+		
+	}
+	elsif ($echoice eq 'n') {
+		$emailbodyfin = $emailbodyplain;
+	} 
+	else {
+		print "\n\tWrong selection my friend";
+	}
 
 	my $mailer = new Net::SMTP::TLS(
 		'smtp.gmail.com',
@@ -150,24 +206,58 @@ sub mail_send {
 		$mailer->mail($emailto);
 		$mailer->to($emailto);
 		$mailer->data;
-		$mailer->datasend($emailbody);
+		$mailer->datasend($emailbodyfin);
 		$mailer->dataend;
 		$mailer->quit;
 		print "\n\tEmail sent.";
 }
 
-sub get_credentials {
+sub mail_encrypt {
 
-        print "\n\tLogin ID: ";
-        my $emaillogin = <STDIN>;
+        print "\n\n\tEncrypt message\n\n";
+
+        print "\n\tPlain text/body: ";
+        $emailbodyplain2 = get_body();
+
+        print "\n\tEncryption key: ";
+        $enckey3 = get_pass();
+
+        my $encryptedmsg1 = encrypt_body($emailbodyplain2, $enckey3);
+
+        print "\n\tEncrypted message: "; print $encryptedmsg1;
+
+}
+
+sub mail_decrypt {
+	
+	print "\n\n\tDecrypt message\n\n";
+
+	print "\n\tEncrypted text/body: ";
+	$emailbodyplain1 = get_body();
+	
+	print "\n\tDecryption key: ";
+	$enckey2 = get_pass();
+
+	my $decryptedmsg1 = decrypt_body($emailbodyplain1, $enckey2);
+	
+	print "\n\tDecrypted message: "; print $decryptedmsg1;
+}
+
+sub get_user {
+   
+	my $emaillogin = <STDIN>;
         chomp($emaillogin);
+		
+	return $emaillogin;
+}
 
-        print "\tPassword: ";
-        ReadMode('noecho');
-        chomp(my $password = <STDIN>);
-        ReadMode(0);
+sub get_pass {
 
-	return ($emaillogin, $password);
+	ReadMode('noecho');
+	chomp(my $password = <STDIN>);
+	ReadMode(0);
+
+	return $password;
 }
 
 sub mail_check_imap {
@@ -181,4 +271,32 @@ sub mail_check_imap {
                                 Ssl => 1,
                                 Uid => 1 )
 	or die "\n\tCould not connect to server, terminating...\n";
+}
+
+sub get_body {
+ 
+        my $emailbody = <STDIN>;
+        chomp($emailbody);
+
+	return $emailbody;
+}
+
+$crypt = Crypt::Lite->new( debug => 0, encoding => 'hex8' );
+
+sub encrypt_body {
+	
+	my ($pbody1, $epass1) = $_;
+	$crypt = Crypt::Lite->new( debug => 0, encoding => 'hex8' );
+	$encrypted = $crypt->encrypt($pbody1, $epass1);
+	
+	return $encrypted;
+}
+
+sub decrypt_body {
+
+        my ($pbody2, $epass2) = $_;
+        $crypt = Crypt::Lite->new( debug => 0, encoding => 'hex8' );
+        $decrypted = $crypt->decrypt($pbody2, $epass2);
+
+        return $decrypted;
 }
