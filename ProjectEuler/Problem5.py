4235b8ebb77f9b8db172ade55860bb35753c4a5c
jsinix <jsinix@users.noreply.github.com>
2014-12-07 17:56:59 -0800

    Create Problem5.py

---
+++ ProjectEuler/Problem5.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+delta = 1
+
+while (delta%1 != 0 or delta%2 != 0 or delta%3 != 0 or delta%4 != 0 or delta%5 != 0 or delta%6 != 0 or delta%7 != 0 or delta%8 != 0 or delta%9 != 0 or delta%10 != 0 or delta%11 != 0 or delta%12 != 0 or delta%13 != 0 or delta%14 != 0 or delta%15 != 0 or delta%16 != 0 or delta%17 != 0 or delta%18 != 0 or delta%19 != 0 or delta%20 != 0):
+    delta = delta+1
+
+print delta
