57cb792160fa629da9c3c7bb8e7571b42f5eac95
jsinix <jsinix@users.noreply.github.com>
2014-12-23 15:34:00 -0500

    Update Problem3.py
    # Question: Largest prime factor
    # Problem 3
    # The prime factors of 13195 are 5, 7, 13 and 29.
    # What is the largest prime factor of the number 600851475143 ?
    # Answer: 6857

--- ProjectEuler/Problem3.py
+++ ProjectEuler/Problem3.py
@@ -1,5 +1,10 @@
-#!/usr/bin/python
+# Question: Largest prime factor
+# Problem 3
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+# Answer: 6857
 
+#!/usr/bin/python
 def primes(n):
     primfac = []
     d = 2
@@ -11,5 +16,4 @@ def primes(n):
     if n > 1:
        primfac.append(n)
     return primfac
-
 print primes(600851475143)
