87f72301ed2acc326c7064918959caab925644d6
jsinix <jsinix@users.noreply.github.com>
2015-08-03 09:39:46 -0400

    Create rssSecNewsFeed.py
    [root]$ ./rssSecNewsFeed.py
    The Hacker News
    
    2015-8-3 12:39:18  Mt.Gox Bitcoin Exchange CEO Arrested by Japanese Police
     - http://thehackernews.com/2015/08/mtgox-bitcoin-exchange.html
    
    2015-8-3 11:14:35  Sadly, Windows 10 Is Stealing Your Bandwidth 'By Default' — Disable It Immediately
     - http://thehackernews.com/2015/08/windows-10-update.html
    
    2015-8-3 7:40:45  Untethered TaiG Jailbreak Tool for iOS 8.4 [Mac OS X version] Released
     - http://thehackernews.com/2015/08/untethered-ios-jailbreak-tool.html
    
    2015-8-1 12:47:4  How Spies Could Unmask Tor Users without Cracking Encryption
     - http://thehackernews.com/2015/08/unmask-tor-network.html
    
    2015-8-1 10:28:14  How to Hack Millions of Android Phones Using Stagefright Bug, Without Sending MMS
     - http://thehackernews.com/2015/07/how-to-hack-android-phone.html
    
    
    
    SecurityFocus Vulnerabilities
    --------------------------------------
    Vuln: redcarpet CVE-2015-5147 Stack Buffer Overflow Vulnerability
    http://www.securityfocus.com/bid/75508
    redcarpet CVE-2015-5147 Stack Buffer Overflow Vulnerability
    
    Vuln: Novius OS 'tab' parameter Local File Include Vulnerability
    http://www.securityfocus.com/bid/75533
    Novius OS 'tab' parameter Local File Include Vulnerability
    
    Vuln: Oracle Java SE CVE-2015-0488 Remote Security Vulnerability
    http://www.securityfocus.com/bid/74111
    Oracle Java SE CVE-2015-0488 Remote Security Vulnerability
    
    Vuln: Debian OpenJDK CVE-2014-8873 Remote Code Execution Vulnerability
    http://www.securityfocus.com/bid/76019
    Debian OpenJDK CVE-2014-8873 Remote Code Execution Vulnerability
    
    Bugtraq: Multiple XSS vulnerabilities in FortiSandbox WebUI
    http://www.securityfocus.com/archive/1/536124
    Multiple XSS vulnerabilities in FortiSandbox WebUI

---
+++ RSS-SecNewsFeed/rssSecNewsFeed.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import calendar, datetime
+import smtplib, errno, sys, re
+import string, os, time, urllib2
+import requests, feedparser
+from bs4 import BeautifulSoup
+from pprint import pprint
+import unicodedata, xmltodict
+
+def secFocusParser():
+    file = urllib2.urlopen('http://www.securityfocus.com/rss/vulnerabilities.xml')
+    data = file.read()
+    file.close()
+    data = xmltodict.parse(data)
+    secFocusFinalNews = ""
+    pageTitle = data['rss']['channel']['title'] + "\n"
+    secFocusFinalNews = secFocusFinalNews + "\n" + pageTitle + "--------------------------------------"
+    for eachPubVul in range(5):
+        eachTitle = data['rss']['channel']['item'][eachPubVul]['title']
+        eachTitle+='\n'
+        eachLink = data['rss']['channel']['item'][eachPubVul]['link']
+        eachLink+='\n'
+        eachDesc = data['rss']['channel']['item'][eachPubVul]['description']
+        eachDesc+='\n'
+        eachJoinNews = eachTitle + eachLink + eachDesc
+        secFocusFinalNews = secFocusFinalNews + "\n" + eachJoinNews
+    return secFocusFinalNews
+
+d = feedparser.parse('http://feeds.feedburner.com/TheHackersNews')
+rssTitle =  d['feed']['title']
+def rssParser(counter):
+    concatString = ""
+    for i in range(counter):
+        title =  d['entries'][i]['title']
+        tm_year = d['entries'][i]['published_parsed'].tm_year
+        tm_mon = d['entries'][i]['published_parsed'].tm_mon
+        tm_mday = d['entries'][i]['published_parsed'].tm_mday
+        tm_hour = d['entries'][i]['published_parsed'].tm_hour
+        tm_min = d['entries'][i]['published_parsed'].tm_min
+        tm_sec = d['entries'][i]['published_parsed'].tm_sec
+        dayz = str(tm_year)+'-'+str(tm_mon)+'-'+str(tm_mday)
+        timez = str(tm_hour)+':'+str(tm_min)+':'+str(tm_sec)
+        newzString = dayz+' '+timez+'  '+title
+        origLink = d['entries'][i]['feedburner_origlink']
+        concatString = concatString+'\n'+ newzString+'\n - '+origLink+'\n'
+    return concatString
+
+thHackNewsTotal = rssTitle + '\n' + rssParser(5)
+print thHackNewsTotal + '\n\n' + secFocusParser()
