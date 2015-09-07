89b3167de2c4ccce7ba1e75c8118c3e4539a235e
jsinix <jsinix@users.noreply.github.com>
2015-09-07 17:23:27 -0400

    Create nixInterfaces.py
    jsinix scripts $ ./nixInterfaces.py
    ['face', 'eth0', 'lo']

---
+++ nixInterfaces/nixInterfaces.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+def getNixInterfaces():
+    with open('/proc/net/dev') as f:
+        allInterfaces = []
+        for line in f:
+            rawIface = line.split()[0]
+            if 'Inter' not in rawIface:
+                if ':' in rawIface:
+                    allInterfaces.append(rawIface[:-1])
+                else:
+                    allInterfaces.append(rawIface)
+        return allInterfaces
+
+print getNixInterfaces()
