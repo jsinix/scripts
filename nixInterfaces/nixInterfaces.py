b5c2239d27649962c78fe5de62ece62bfbf73811
jsinix <jsinix@users.noreply.github.com>
2015-09-07 17:32:51 -0400

    Update nixInterfaces.py

--- nixInterfaces/nixInterfaces.py
+++ nixInterfaces/nixInterfaces.py
@@ -6,10 +6,11 @@ def getNixInterfaces():
         for line in f:
             rawIface = line.split()[0]
             if 'Inter' not in rawIface:
-                if ':' in rawIface:
-                    allInterfaces.append(rawIface[:-1])
-                else:
-                    allInterfaces.append(rawIface)
+                if 'face' not in rawIface:
+                    if ':' in rawIface:
+                        allInterfaces.append(rawIface[:-1])
+                    else:
+                        allInterfaces.append(rawIface)
         return allInterfaces
 
 print getNixInterfaces()
