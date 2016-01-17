0277fe0f096c7d1dd759105db7e978f92afc4f21
jsinix <jsinix@users.noreply.github.com>
2016-01-17 10:50:40 -0500

    Update jsinix_linkedin_miner.py

--- LinkedINMiner/jsinix_linkedin_miner.py
+++ LinkedINMiner/jsinix_linkedin_miner.py
@@ -1,21 +1,14 @@
-# Permission to use, copy, modify and distribute this 
-# software and its documentation for any purpose and 
-# without fee is hereby granted, provided that the above 
-# copyright notice appear in all copies that both 
-# copyright notice and this permission notice appear in 
-# supporting documentation. jsinix makes no representations 
-# about the suitability of this software for any purpose. 
-# It is provided "as is" without express or implied warranty.
-
-# jsinix DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, 
-# INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. 
-# IN NO EVENT SHALL jsinix BE LIABLE FOR ANY SPECIAL, INDIRECT 
-# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
-# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
-# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
 #!/usr/bin/python
+
+# Disclaimer: This script is only for educational purposes.
+# Please use this at your own risk.
+# Author: jsinix(jsinix.1337@gmail.com) 
+
+# This script is used to mine data from linkedin. It gets the information 
+# about your connections. The functionality of the script can be improved 
+# as per your liking. This is just a POC and the LinkedIN API can be used 
+# to do more stuff. 
+
 from linkedin import linkedin
 import json, os.path, argparse
 from prettytable import PrettyTable
@@ -53,9 +46,7 @@ USER_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
 fname = "linkedin_connections.json"
 yonbit = os.path.isfile(fname) 
 
-
 def get_user_auth():
-    
     RETURN_URL = ''
     auth = linkedin.LinkedInDeveloperAuthentication(CONSUMER_KEY, CONSUMER_SECRET,
                                     USER_TOKEN, USER_SECRET,
@@ -65,9 +56,7 @@ def get_user_auth():
     app = linkedin.LinkedInApplication(auth)
     return app
 
-
 def get_data_from_linkedin():
-    
     app = get_user_auth()
     print "(+) Grabbing all contacts"
     connections = app.get_connections()
@@ -79,9 +68,7 @@ def get_data_from_linkedin():
     except Exception as ee:
 	print "(+) Error: ", ee
 
-
 def data_already_present(): 
-    
     print "(+) Reading data"
     connections = json.loads(open(fname).read())
     print "(+) Parsing data \n\n"
@@ -92,18 +79,14 @@ def data_already_present():
           if c.has_key('location')]
     print pt
 
-
 def download_url(uri02, fname1, lname1):
-   
     print "(+) Downloading Picture: %s %s" % (fname1, lname1)
     file1 = urllib.URLopener()
     path = urlparse.urlparse(uri02).path
     fn = fname1+lname1
     file1.retrieve(uri02, fn)
 
-
 def download_linledin_pictures(looper1):
-    
     connections = json.loads(open(fname).read())
     if connections['values'][looper1]['pictureUrl'] :
        try:
@@ -112,7 +95,6 @@ def download_linledin_pictures(looper1):
            print "(+) Error: %s %s :: %s" %(connections['values'][looper1]['firstName'], connections['values'][looper1]['lastName'], e)
 
 def get_pictures():
-
     connections = json.loads(open(fname).read())
     for zz in range(len(connections['values'])):
         try:
@@ -122,7 +104,6 @@ def get_pictures():
 	    print "(+) Error: %s :: pictureUrl" % e
 
 def list_all():
-    
     try: 
         if yonbit == False:
 	    get_data_from_linkedin(get_user_auth())	
@@ -131,7 +112,6 @@ def list_all():
     except Exception as e:
 	print "Error: ", e			
 
-
 def print_Connection_details(looper1):
     connections = json.loads(open(fname).read())
     print "Name: ", connections['values'][looper1]['firstName'], connections['values'][looper1]['lastName']
@@ -139,13 +119,10 @@ def print_Connection_details(looper1):
     print "Industry: ", connections['values'][looper1]['industry']
     print "Location: ", connections['values'][looper1]['location']['name']
 
-
 def get_details():
-
     if yonbit == False:
 	print "(+) The parsed file is not present locally. \n(+) Use -g or --get to get the file first."
         get_data_from_linkedin()
-  
     connections = json.loads(open(fname).read())
     for zz in range(len(connections['values'])):
         try:
@@ -154,35 +131,28 @@ def get_details():
         except Exception as e:
             pass
 
-
 def process_arguments(args):
     parser = argparse.ArgumentParser(description="This is LinkedIN mining tool. Use it wisely")
-    
     parser.add_argument('-l',
 			'--list-all',
 			action='store_true',
 			help="Lists all the connections along with location in a table"
                         )
-
     parser.add_argument('-d',
 		        '--details',
 		        action='store_true',
 			help='Lists all the connections with the details of each'
 		        )
-
     parser.add_argument('-p',
                         '--picture',
                         action='store_true',
                         help='Download the pictures of all the possible connections'
                         )
-
     parser.add_argument('-g',
                         '--get',
                         action='store_true',
                         help='Dumps the entire data locally in filename linkedin_connections.json'
                         )
-	
-
     parser.add_argument('-v',
 			'--verbose',
 			dest='verbose',
@@ -190,22 +160,17 @@ def process_arguments(args):
 			default=False,
 			help='Displays all the information'
 			)
-
     options = parser.parse_args(args)
     return vars(options)
 
 if len(sys.argv) < 2:
     process_arguments(['-h'])
 userOptions = process_arguments(sys.argv[1:])
-
 if userOptions["get"] == True:
     get_data_from_linkedin()
-
 if userOptions["list_all"] == True:
     list_all()
-
 if userOptions["details"] == True:
     get_details()
-
 if userOptions["picture"] == True:
     get_pictures()
