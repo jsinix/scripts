262075b51f3fae8bb5c38d0456ecd2fc408f197b
jsinix <jsinix@users.noreply.github.com>
2016-01-17 10:33:45 -0500

    Update CBSE_Results_Dumper.py

--- CBSE-Result-Dumper/CBSE_Results_Dumper.py
+++ CBSE-Result-Dumper/CBSE_Results_Dumper.py
@@ -22,8 +22,7 @@ from multiprocessing import Pool
 # Disclaimer: This script is only for educational purposes.
 # Please use this at your own risk.
 # Author: jsinix(jsinix.1337@gmail.com) 
-
-
+#
 # This script runs to grab results of all the roll numbers 
 # range passed as a range. This can be used to dump all the 
 # possible roll numbers possible. There is just one that is 
@@ -32,11 +31,9 @@ from multiprocessing import Pool
 # If you view the source of the page you will notice that 
 # the valid roll numbers start with 16, 17, ..... 99. So 
 # you can start with that.
-
 # Once the entire database id dumped, you can reverse search
 # someone's details by just the name by 'grep' etc if you do
 # not know someone's roll number.
-
 # In addition to that, this script uses parallel processing
 # to send multiple requests simultaniously to save time.
 
@@ -58,7 +55,6 @@ def result_grabber(rollx):
 if __name__ == '__main__':
     pool = Pool(processes=20)
     input1 = range(1600000,1600100)
-
     try:
         result = pool.map(result_grabber, input1)
     except:
