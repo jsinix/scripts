48d794c8753a9e630a799cba82fc8ee792eab69d
jsinix <jsinix@users.noreply.github.com>
2016-01-17 10:51:28 -0500

    Update MAC_Vendor_name.py

--- MAC-Vendor-Name/MAC_Vendor_name.py
+++ MAC-Vendor-Name/MAC_Vendor_name.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 import urllib
 
-#This tiny piece of code resolves the MAC address to its vendor. 
+# Disclaimer: This script is only for educational purposes.
+# Please use this at your own risk.
+# Author: jsinix(jsinix.1337@gmail.com) 
 
+#This tiny piece of code resolves the MAC address to its vendor. 
 def Get_Vendor(macaddr):
     connection = urllib.urlopen("http://api.macvendors.com/"+macaddr)
     output = connection.read()
@@ -10,5 +13,4 @@ def Get_Vendor(macaddr):
     return output
 
 my_mac = "xx:xx:xx:xx:xx:xx"
-
 print Get_Vendor(my_mac)
