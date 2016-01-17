8220a2b69a357aa40a922555b41e9b609759d390
jsinix <jsinix@users.noreply.github.com>
2016-01-17 10:47:48 -0500

    Update jsinix_spamhaus_blacklist.py

--- IptablesSpamhaus/jsinix_spamhaus_blacklist.py
+++ IptablesSpamhaus/jsinix_spamhaus_blacklist.py
@@ -1,22 +1,20 @@
-# Permission to use, copy, modify and distribute this 
-# software and its documentation for any purpose and 
-# without fee is hereby granted, provided that the above 
-# copyright notice appear in all copies that both 
-# copyright notice and this permission notice appear in 
-# supporting documentation. jsinix makes no representations 
-# about the suitability of this software for any purpose. 
-# It is provided "as is" without express or implied warranty.
-
-# jsinix DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, 
-# INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. 
-# IN NO EVENT SHALL jsinix BE LIABLE FOR ANY SPECIAL, INDIRECT 
-# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
-# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
-# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
 #!/usr/bin/python
-#This script if used with a cronjob can be useful.
+
+# Disclaimer: This script is only for educational purposes.
+# Please use this at your own risk.
+# Author: jsinix(jsinix.1337@gmail.com) 
+
+# I wrote this script to setup basic iptable rules to secure the system.
+# In addition to that, this script queries spamhaus's blacklisted IP/Network
+# addresses. These IP's are then stored in a new chain called droplist. Finally
+# it is referenced in the default filter table chains(i.e INPUT, OUTPUT and FORWARD).
+# This script or its customized version can be useful for many type of public 
+# facing servers including mail servers to protect from spams etc.
+# Please use this at yur own risk and read carefully before using. You might 
+# need to change some parts according to your needs.
+# This script if used with a cronjob will update your machine with
+# new blocklisted IP's
+
 import requests
 from netaddr import *
 import subprocess, getpass
@@ -118,8 +116,7 @@ def controller():
     use_blocklist()
 
 # This script must be run as root to avoid permission
-# issues.
-#So lets make sure that no other user can run it.
+# issues. So lets make sure that no other user can run it.
 my_user = getpass.getuser()
 if(my_user != 'root'):
     print "(+) Please run this script as ROOT"
